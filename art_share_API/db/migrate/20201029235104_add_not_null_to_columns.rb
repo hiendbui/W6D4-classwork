d25ccab96ed51ce4af8b1eb875a6b7e18d5c7fc0
hiendbui <hiendbui0@gmail.com>
2020-10-29 17:33:45 -0700

    Add artwork_shares and comments

---
+++ art_share_API/db/migrate/20201029235104_add_not_null_to_columns.rb
@@ -0,0 +1,7 @@
+class AddNotNullToColumns < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :comments, :body, false
+    change_column_null :comments, :user_id, false
+    change_column_null :comments, :artwork_id, false
+  end
+end
--- art_share_API/db/schema.rb
+++ art_share_API/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_214821) do
+ActiveRecord::Schema.define(version: 2020_10_29_235104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2020_10_29_214821) do
     t.index ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "user_id", null: false
+    t.bigint "artwork_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artwork_id"], name: "index_comments_on_artwork_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
@@ -38,4 +48,6 @@ ActiveRecord::Schema.define(version: 2020_10_29_214821) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "artworks"
+  add_foreign_key "comments", "users"
 end
